fd5df8f25df134e1a47ce280ad374ab41f8bb4d9
annazhenwang <annazhenwang@gmail.com>
2014-11-04 17:17:57 -0800

    add a table for different ions

---
+++ calculate_magnetic.py
@@ -0,0 +1,10 @@
+import sys, os, time, math, array
+r= 57 #cm
+U = 7 #kV
+mass_charge = [1, 2, 8, 20, 28, 32, 40 ] # mass charge ratio
+f = open("magnetic field.txt", "w")
+for i in range(len(mass_charge)):
+    B = math.sqrt(2)/1.0e-2*math.sqrt(1.67e-27/1.6e-19)*math.sqrt(1.0e3)*1/r*math.sqrt(mass_charge[i])*math.sqrt(U)
+    print( "%g" % B)
+    f.write(str(B)+ "\n")
+f.close()
